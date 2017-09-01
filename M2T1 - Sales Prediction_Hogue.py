f0c6542eace2effc61821373595db23182eeffb8
Hoguey <31550799+Hoguey@users.noreply.github.com>
2017-09-01 16:05:52 -0400

    Add files via upload

---
+++ M2T1 - Sales Prediction_Hogue.py
@@ -0,0 +1,19 @@
+# CTI 110
+# M2T1- Sales Prediction
+# Brandon Hogue
+# 9/1
+
+
+
+
+# Projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
+
+
